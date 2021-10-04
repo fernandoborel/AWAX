44b863b17861d716197f2c3dbbf6691d3b0e378c
Fernando Borel <fernandomenezesborel@gmail.com>
2021-10-03 21:47:24 -0300

    Adicionado novos arquivos

---
+++ script.js
@@ -0,0 +1,18 @@
+function subirtela() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+function decidirbotaoscroll() {
+    if(window.scrollY === 0) {
+        // ocultar o botão
+        document.querySelector('.scrollbutton').style.display = 'none';
+    } else {
+        // mostrar o botão
+        document.querySelector('.scrollbutton').style.display = 'block';
+    }
+}
+
+setInterval(decidirbotaoscroll, 1000);
